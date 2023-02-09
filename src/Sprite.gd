68d4322d9fb2df41aba89f0f9f2f1370fb55977f
Rasmus Stougaard <rst@eucnordvest.dk>
2023-02-09 14:18:29 +0100

    Import code from prototype

---
+++ src/Sprite.gd
@@ -0,0 +1,20 @@
+extends Sprite
+
+var angular_speed = PI
+var speed = 400.0
+
+func _process(delta: float) -> void:
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += direction * angular_speed * delta
+
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+
+	position += velocity * delta
+
